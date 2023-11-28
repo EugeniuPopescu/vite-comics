8eeb20ee94f2a54556768d4822b7f089f3c17208
Eugen <epopescuu@yahoo.it>
2023-11-28 17:25:34 +0100

    feat: add FooterTop component

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -13,7 +13,7 @@ export default {
 </script>
 
 <template>
-  
+    <FooterTop />
 </template>
 
 <style scoped>
--- src/components/FooterTop.vue
+++ src/components/FooterTop.vue
@@ -1,22 +1,62 @@
 <script>
 export default {
-    name: "FooterTop",
+    name: "AppHeader",
+    data() {
+        return {
+            cards: [
+                {
+                    img: "/public/img/buy-comics-digital-comics.png",
+                    text: "digital comics",
+                },
+                {
+                    img: "/public/img/buy-comics-merchandise.png",
+                    text: "dc merchandise",
+                },
+                {
+                    img: "/public/img/buy-comics-subscriptions.png",
+                    text: "subscription",
+                },
+                {
+                    img: "/public/img/buy-comics-shop-locator.png",
+                    text: "comic shop locator",
+                },
+                {
+                    img: "/public/img/buy-dc-power-visa.svg",
+                    text: "dc power visa",
+                },
+            ]
+        }
+    },
+    mounted() {
+        console.log("Componente FooterTop caricato");
+    }
 }
-    
 </script>
 
 <template>
-<div class="bg-dark">
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <span class="text-light fs-2"></span>
-            </div>
+<div class="footer-top text-light" >
+    <div class="container d-flex justify-content-between align-items-center px-4">
+        <div 
+        v-for="card in cards"
+        class="my-4">
+            <img :src="card.img" alt="">
+            <a class="mx-2">{{ card.text.toLocaleUpperCase() }}</a>
         </div>
     </div>
 </div>
 </template>
 
 <style scoped>
+.footer-top {
+    background-color: #3980F1;;
+}
 
+img {
+    width: 3rem;
+}
+
+a {
+    text-decoration: none;
+    color: white;
+}
 </style>
