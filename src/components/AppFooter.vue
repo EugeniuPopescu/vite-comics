0e0a5e08579adbfa423c2083c95c8dc594ab2ccf
Eugen <epopescuu@yahoo.it>
2023-11-28 17:00:58 +0100

    help: component in component

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,5 +1,15 @@
 <script>
+import FooterTop from './FooterTop.vue';
+import FooterMiddle from './FooterMiddle.vue';
+import FooterBottom from './FooterBottom.vue';
 
+export default {
+	components: {
+		FooterTop,
+		FooterMiddle,
+		FooterBottom,
+	}
+}
 </script>
 
 <template>
---
+++ src/components/FooterTop.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    name: "FooterTop",
+}
+    
+</script>
+
+<template>
+<div class="bg-dark">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <span class="text-light fs-2"></span>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<style scoped>
+
+</style>
